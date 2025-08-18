fa203cef41aa0912380cd15f2b3cedba621c6e57
Vikas Yadav <yvikas2006@gmail.com>
2025-08-18 15:10:58 +0530

    Create UpperCase.java

---
+++ Basics String/UpperCase.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class UpperString{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any string ");
+		String str=sc.nextLine();
+		System.out.print(str.toUpperCase());
+	}
+
+}
