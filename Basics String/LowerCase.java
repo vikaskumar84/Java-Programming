681e6935a3642c0d9ec679b033401b10ac667212
Vikas Yadav <yvikas2006@gmail.com>
2025-08-18 18:45:47 +0530

    Create LowerCase.java

---
+++ Basics String/LowerCase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class LowerString{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any string ");
+		String str=sc.nextLine();
+		System.out.println("original string :"+str);
+		System.out.println("lower string :"+str.toLowerCase());
+	}
+
+}
